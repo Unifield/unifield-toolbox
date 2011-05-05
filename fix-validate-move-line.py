4ce797ad7bbba1f4b20ec2726eb9f54783658688
jf <jf@tempo4>
2011-05-05 15:06:11 +0200

    [FIX] ident

--- fix-validate-move-line.py
+++ fix-validate-move-line.py
@@ -11,14 +11,12 @@ sock = xmlrpclib.ServerProxy('http://127.0.0.1:8069/xmlrpc/object')
 model = 'account.move'
 ids = sock.execute(dbname, uid, pwd, model, 'search', [])
 for move in sock.execute(dbname, uid, pwd, model, 'read',ids, ['line_id','name','ref']):
-	ssum = 0
-	lines = []
-	if move['line_id']:
-		for line in sock.execute(dbname, uid, pwd,  'account.move.line', 'read',move['line_id'], ['debit','credit','state']):
-			ssum += line['debit'] - line['credit']
-			lines.append(line)
-	if ssum >= 0.0001:
-        print 'Fixed'
-		sock.execute(dbname, uid, pwd,  'account.move.line', 'write', [l['id'] for l in lines], {})
-
-
+    ssum = 0
+    lines = []
+    if move['line_id']:
+        for line in sock.execute(dbname, uid, pwd,  'account.move.line', 'read',move['line_id'], ['debit','credit','state']):
+            ssum += line['debit'] - line['credit']
+            lines.append(line)
+    if ssum >= 0.0001:
+        print 'FIX'
+        sock.execute(dbname, uid, pwd,  'account.move.line', 'write', [l['id'] for l in lines], {})
