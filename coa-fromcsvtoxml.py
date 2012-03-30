a231e813345634995fd10ea220461a8fe6065129
jf <jf@tempo4>
2012-03-30 17:50:16 +0200

    New coa

--- coa-fromcsvtoxml.py
+++ coa-fromcsvtoxml.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 import csv
 
-out = 'coa.xml'
-inf = 'coa.csv'
+out = '/tmp/add.xml'
+inf = '/tmp/add.csv'
 fo = open(out, 'w')
 
 reader = csv.reader(open(inf, "rb"),delimiter=";",lineterminator="\r",quotechar='"')
@@ -22,11 +22,18 @@ acctype = {
     'expense': 'account.account_type_expense',
     'income': 'account.account_type_income',
     'stock': 'account_activable.account_type_stock',
+    'equity': 'account.account_type_cash_equity',
 }
 
 ttype = {
     'regular': 'other'
 }
+
+type_for_register = [('none', 'None'), ('transfer', 'Transfer'), ('transfer_same','Transfer (same currency)'), ('advance', 'Cash Advance')]
+type_for_reg_dic ={}
+for a,b in type_for_register:
+    type_for_reg_dic[b.lower()] = a
+
 def toxml(val):
     return val.replace('&', '&amp;').replace('<','&lt;').replace('>','&gt;')
 
@@ -44,7 +51,7 @@ for row in reader:
             <field name="reconcile" eval="%s" />
             <field name="user_type" ref="%s" />
         </record>
-'''%(row[0], row[0], row[5].lower(), ttype.get(row[3].lower(), row[3].lower()), toxml(row[1]), row[2] and 'ref="msf_chart_of_account.%s"'%row[2] or '', row[6] == 'VRAI' and 'True' or 'False', acctype[row[4]] ))
+'''%(row[0], row[0], type_for_reg_dic[row[5].lower()], ttype.get(row[3].lower(), row[3].lower()), toxml(row[1]), row[2] and 'ref="msf_chart_of_account.%s"'%row[2] or '', row[6] == 'VRAI' and 'True' or 'False', acctype[row[4].lower()] ))
 
 fo.write('''    </data>
 </openerp>''')
