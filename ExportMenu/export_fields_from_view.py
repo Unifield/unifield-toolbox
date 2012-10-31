0b9eee652e73cb5dad91ff809d96284519a62f6a
jf <jf@ubuntu>
2012-10-31 13:36:47 +0100

    [IMP] export object fields

--- ExportMenu/export_fields_from_view.py
+++ ExportMenu/export_fields_from_view.py
@@ -3,12 +3,15 @@ import xmlrpclib
 import csv
 import sys
 from lxml import etree
-
+from mako.template import Template
+import zipfile
+import shutil
 
 dbname='jfb-wm2'
 user='admin'
 pwd = 'admin'
 port = 7069
+out_file = 'registers_fields.ods'
 
 sock = xmlrpclib.ServerProxy('http://127.0.0.1:%s/xmlrpc/common'%(port, ))
 uid = sock.login(dbname, user, pwd)
@@ -19,7 +22,9 @@ model='ir.ui.menu'
 
 
 
-def write_fields(csv, arch, fields, level=0):
+
+
+def write_fields(d, arch, fields, level=0):
     field = arch.get('name')
     info = fields[field]
 
@@ -40,7 +45,7 @@ def write_fields(csv, arch, fields, level=0):
     if level:
         begin ='%s ' % ('-'*level, )
 
-    csv.writerow([
+    d.append([
         '%s%s' % (begin,field),
         arch.get('string', info['string']),
         arch.get('help', info.get('help', '')),
@@ -51,10 +56,11 @@ def write_fields(csv, arch, fields, level=0):
     ])
 
 
-csvf = csv.writer(open('menu.csv', 'wb'), delimiter=';', quotechar='"')
-csvf.writerow(['Field', 'Name', 'Help', 'Type', 'Invisble', 'RO', 'Rq'])
+headers = ['Field', 'Name', 'Help', 'Type', 'Invisble', 'RO', 'Rq']
+ods_datas = []
 xmlids = [('account', 'menu_bank_statement_tree'), ('register_accounting', 'menu_cash_register'), ('account', 'journal_cash_move_lines')]
 for module, xmlid in xmlids:
+    tmp_data = {}
     data_ids = sock.execute(dbname, uid, pwd, modeldata, 'search', [('module', '=', module), ('name', '=', xmlid)])
     datas = sock.execute(dbname, uid, pwd, modeldata, 'read', data_ids, ['res_id', 'model'])
     if datas[0]['model'] != 'ir.ui.menu':
@@ -65,7 +71,7 @@ for module, xmlid in xmlids:
     form_id = False
     action = sock.execute(dbname, uid, pwd, act, 'read', int(act_id), ['views', 'res_model'])
     res_model = action['res_model']
-    csvf.writerow([menu['name']])
+    tmp_data['name'] = menu['name']
     for views in action['views']:
         if views[1] == 'form':
             form_id = views[0]
@@ -82,56 +88,9 @@ for module, xmlid in xmlids:
                 write_fields(csvf, arch_field2, info['views']['tree']['fields'], 1)
 
 
+shutil.copyfile('template/registers_fields.ods', out_file)
+zip = zipfile.ZipFile(out_file, 'a')
+mytemplate = Template(filename='template/content.xml', output_encoding='utf-8', input_encoding='utf-8')
+zip.writestr('content.xml', mytemplate.render(headers=headers, objects=datas)
+zip.close()
 
-
-
-sys.exit(1)
-groups_id = sock.execute(dbname, uid, pwd, 'res.groups','search',[('name','!=','Tempo')])
-groups=[]
-for group in sock.execute(dbname, uid, pwd, 'res.groups','read',groups_id,['name']):
-    groups.append( (group['id'],group['name']) )
-
-csvf.writerow(['module','xml_id','Level','Name', 'Object', 'Technical object name']+[x[1] for x in groups])
-
-cache_obj = {}
-def printlevel(parent=False, level=0,groupherit=[]):
-    ids = sock.execute(dbname, uid, pwd, model, 'search',[('parent_id','=', parent)])
-    if ids:
-       for m in  sock.execute(dbname, uid, pwd, model, 'read', ids, ['name', 'groups_id', 'action'], {'lang': 'fr_FR'}):
-        rids = sock.execute(dbname, uid, pwd, modeldata, 'search', [('res_id', '=', m['id']), ('model', '=', model)])
-        data = sock.execute(dbname, uid, pwd, modeldata, 'read', rids[0], ['name', 'module'])
-        # search action
-        human_obj = ''
-        technical_obj = ''
-        if m['action']:
-            act_type, action_id = m['action'].split(',')
-            if act_type != 'ir.actions.server':
-                new_obj = sock.execute(dbname, uid, pwd, act_type, 'read', [action_id], ['res_model'])[0]
-                technical_obj = new_obj['res_model']
-                if technical_obj not in cache_obj:
-                    model_ids = sock.execute(dbname, uid, pwd, 'ir.model', 'search', [('model', '=', technical_obj)])
-                    cache_obj[new_obj['res_model']] = sock.execute(dbname, uid, pwd, 'ir.model', 'read', model_ids[0], ['name'])['name']
-                human_obj = cache_obj[technical_obj]
-            else:
-                human_obj = sock.execute(dbname, uid, pwd, act_type, 'read', [action_id], ['name'])[0]['name']
-
-        add = []
-        for g in groups:
-            if g[0] in m['groups_id']:
-                add.append(g[1])
-            #elif g[0] in groupherit:
-            #    add.append("(%s)"%g[1])
-            else:
-                add.append('')
-
-        csvf.writerow([data['module'],data['name'],'+'*level,m['name'].encode('utf8'), human_obj, technical_obj]+add)
-        printlevel(m['id'], level+1, m['groups_id'] or groupherit)
-
-printlevel()
-
-#print sock.execute(dbname, uid, pwd, 'res.partner', [])
-#print ids
-#print sock.execute(dbname, uid, pwd, model, 'perm_read', [1],['write_date'])
-#print sock.execute(dbname, uid, pwd, model, 'read', [1],['content'])[0]['content']
-
-#print sock.execute(dbname, uid, pwd, model, 'read',ids,[date])
