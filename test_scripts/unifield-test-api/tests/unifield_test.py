411fd2342f1ca7be6592757327c8cc21646f0f43
Olivier DOSSMANN <od@tempo-consulting.fr>
2014-03-11 16:34:23 +0100

    [IMP] Unifield-tests - Check that unifield_tests module is loaded. If not, install it.

--- test_scripts/unifield-test-api/tests/unifield_test.py
+++ test_scripts/unifield-test-api/tests/unifield_test.py
@@ -19,9 +19,28 @@ class UnifieldTest(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         super(UnifieldTest, self).__init__(*args, **kwargs)
+        # Keep each database connection
         self.sync = XMLConn('SYNC_SERVER')
         self.hq1 = XMLConn('HQ1')
         self.c1 = XMLConn('HQ1C1')
         self.p1 = XMLConn('HQ1C1P1')
+        # For each database, check that unifield_tests module is loaded
+        #+ If not, load it.
+        module_to_install = 'unifield_tests'
+        for database_name in ['hq1', 'c1', 'p1']:
+            database = getattr(self, database_name)
+            module_obj = database.get('ir.module.module')
+            m_ids = module_obj.search([('name', '=', module_to_install)])
+            for module in module_obj.read(m_ids, ['state']):
+                state = module.get('state', '')
+                if state == 'uninstalled':
+                    module_obj.button_install(module.get('id'))
+                    database.get('base.module.upgrade').upgrade_module([])
+                elif state in ['to upgrade', 'to install']:
+                    database.get('base.module.upgrade').upgrade_module([])
+                elif state in ['installed']:
+                    pass
+                else:
+                    raise EnvironmentError('Wrong module state: %s' % (state or '',))
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
