3295af87ddbeadccbd20adbcded2d8d43aa62b29
Olivier DOSSMANN <od@tempo-consulting.fr>
2014-03-11 11:29:52 +0100

    [ADD] Unifield-tests - Do not execute a given method as test in inheritance

--- test_scripts/unifield-test-api/tests/test_02.py
+++ test_scripts/unifield-test-api/tests/test_02.py
@@ -1,6 +1,15 @@
 import test_01
+import unittest
 
 class AnotherTest(test_01.SomethingTest):
+
+    ###########################################################################
+    ## We skip test_020 because it was already done in SomethingTest class.
+    ## We consider that test_020 should not be executed twice (or more)
+    ###########################################################################
+    @unittest.skip('Already used')
+    def test_020_sentence(self):
+        super(AnotherTest.test_020_sentence())
 
     def test_030_after(self):
         '''Method used after all others methods'''
