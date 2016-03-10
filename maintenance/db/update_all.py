09221ab8ccd4b5c574eef095adbfff609a09a859
docker <docker@f6449f016d01>
2016-03-10 20:30:07 +0000

    [IMP] Manage timeouts

--- maintenance/db/update_all.py
+++ maintenance/db/update_all.py
@@ -1,12 +1,16 @@
 # -*- encoding: utf-8 -*-
 import argparse
 import oerplib
+import time
+import socket
+from threading import Thread
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--host", "-H", metavar="host", default="127.0.0.1", help="Host [default: %(default)s]")
 parser.add_argument("--port", "-p", metavar="port", default=None, help="HTTP/XMLRPC Port [default: 8061/8069 (depending on protocol used)]")
 parser.add_argument("--user", "-u", metavar="user", default="admin", help="User [default: %(default)s]")
 parser.add_argument("--password", "-w", metavar="pwd", default="admin", help="Password [default: %(default)s]")
+parser.add_argument("--exclude", "-x", metavar="exclude", default=False, help="Comma separated list of dbs to exclude")
 o = parser.parse_args()
 
 user= o.user
@@ -19,10 +23,23 @@ if o.port is None:
         o.port = 8061
 port = o.port
 
-oerp = oerplib.OERP(host, protocol='xmlrpc', port=port)
-for db in oerp.db.list():
-    user = oerp.login(user, pwd, db)
-    mod_obj = oerp.get('ir.module.module')
-    mod_ids = mod_obj.search([('name', '=', 'base')])
-    mod_obj.button_upgrade(mod_ids)
-    bmu = oerp.get('base.module.upgrade').upgrade_module([])
+exclude = []
+if o.exclude:
+    exclude = o.exclude.split(',')
+
+if __name__ == '__main__':
+    jobs = []
+    oerp = oerplib.OERP(host, protocol='xmlrpc', port=port)
+    for db in oerp.db.list():
+        if db in exclude:
+            continue
+        uid = oerp.login(user, pwd, db)
+        mod_obj = oerp.get('ir.module.module')
+        mod_ids = mod_obj.search([('name', '=', 'base')])
+        mod_obj.button_upgrade(mod_ids)
+        try:
+            oerp.get('base.module.upgrade').upgrade_module([])
+        except socket.timeout:
+            print 'Time Out. Go to next DB'
+
+    print "Update DBs done"
