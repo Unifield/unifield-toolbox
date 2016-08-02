c5f768e775b86628d0b2a7c8ae1a0730fb75967b
jftempo <jfb@tempo-consulting.fr>
2016-08-02 21:27:42 +0200

    Delete RB:killall

--- RB/RBTools/deleteRB.sh
+++ RB/RBTools/deleteRB.sh
@@ -9,6 +9,8 @@ fi
 /etc/init.d/$1-web stop
 update-rc.d -f $1-server remove
 update-rc.d -f $1-web remove
+killall -u $1
+killall -s 9 -u $1
 rm /etc/apache2/sites-enabled/$1
 /etc/init.d/apache2 reload
 for i in  `psql -t -d template1 -c "SELECT d.datname FROM pg_catalog.pg_database d WHERE pg_get_userbyid(d.datdba) = '$1';"`; do 
