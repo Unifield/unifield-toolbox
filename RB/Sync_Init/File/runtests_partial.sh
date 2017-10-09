08d5081da35825d009b7e37b8c181265bc191e3a
jftempo <jfb@tempo-consulting.fr>
2017-10-09 22:48:26 +0200

    Creadntial msf

--- RB/Sync_Init/File/runtests_partial.sh
+++ RB/Sync_Init/File/runtests_partial.sh
@@ -30,6 +30,28 @@ SERVER_HWID = ''
 USING_DOCKER = False
 """ > testfield/credentials.py
 
+echo """
+DB_PREFIX = '$USER'
+SRV_ADDRESS = '%s.rb.unifield.org' % DB_PREFIX
+# Configuration variables 
+XMLRPC_PORT = $XMLRPC_PORT
+NETRPC_PORT = $NETRPC_PORT
+HTTP_PORT = $WEB_PORT
+HTTP_URL_SERVER = 'http://%s:%d' % (SRV_ADDRESS, HTTP_PORT)
+
+# Configuration variable to generate input files / Restore dumps
+DB_ADDRESS = '127.0.0.1'
+DB_PORT = 12114
+DB_USERNAME = 'toto'
+DB_PASSWORD = 'toto'
+
+UNIFIELD_ADMIN = 'admin'
+UNIFIELD_PASSWORD = 'admin'
+
+SERVER_HWID = ''
+USING_DOCKER = False
+""" > credentials-msf.py
+
 WEBDIR=/home/$USER/unifield-web
 SERVERDIR=/home/$USER/unifield-server
 
