71087626c18d8d2042d518a3dbaa78f0367b0d38
jf <jfb@tempo-consulting.fr>
2019-09-26 17:35:05 +0200

    ContinuousBck

---
+++ ContinuousBackup/key_gen.py
@@ -0,0 +1,77 @@
+#!/cygdrive/c/TMPJFB/crypto-env/bin/python3
+from Crypto.PublicKey import RSA
+import sys
+import zipfile
+import os
+from email.message import EmailMessage
+import smtplib
+
+authorized_keys = '/home/backup/.ssh/authorized_keys'
+keys_dir = '/cygdrive/d/continuous_backup_data/ssh_keys'
+smtp_host = '80.12.95.139'
+
+if not sys.argv or len(sys.argv) != 2:
+    print('%s instance' % sys.argv[0])
+    sys.exit(1)
+
+instance = sys.argv[1]
+auth_read = open(authorized_keys, 'r')
+line = 0
+for x in auth_read:
+    line += 1
+    if x.strip().endswith(instance):
+        print ('%s found in authorized_keys, line %s' % (instance, line))
+        sys.exit(1)
+auth_read.close()
+
+if not os.path.exists(keys_dir):
+    os.makedirs(keys_dir)
+zip_file = os.path.join(keys_dir, '%s.zip'%instance)
+
+if os.path.exists(zip_file):
+    print('%s already exists'  % (zip_file, ))
+    sys.exit(1)
+
+zip_desc = open(zip_file, 'wb')
+zip_doc = zipfile.ZipFile(zip_desc, mode='w', compression=zipfile.ZIP_DEFLATED)
+
+key = RSA.generate(1024)
+zip_doc.writestr('SSH_CONFIG/id_rsa', key.exportKey('PEM'))
+
+pubkey = key.publickey()
+exported_pub_key = pubkey.exportKey('OpenSSH')
+zip_doc.writestr('SSH_CONFIG/id_rsa.pub', exported_pub_key)
+
+
+zip_doc.writestr('SSH_CONFIG/config', """
+IdentityFile "C:\Program Files (x86)\msf\SSH_CONFIG\id_rsa"
+StrictHostKeyChecking no
+UserKnownHostsFile "C:\Program Files (x86)\msf\SSH_CONFIG\knownhosts"
+""")
+zip_doc.close()
+zip_desc.close()
+
+auth = open(authorized_keys, 'ab')
+b_instance =  bytes(instance, 'utf-8')
+auth.write(b'### %s\n' % b_instance)
+auth.write(b'command="rsync --server -vlogDtr --no-perms --chmod=Dg+rwx,Fg+rw --remove-source-files --partial-dir=.rsync-partial --partial . %s/",no-agent-forwarding,no-port-forwarding,no-pty,no-user-rc,no-X11-forwarding %s %s\n' % (b_instance, exported_pub_key, b_instance))
+auth.close()
+
+# Create the container email message.
+msg = EmailMessage()
+msg['Subject'] = 'SSH Key %s' % instance
+# me == the sender's email address
+# family = the list of all recipients' email addresses
+msg['From'] = 'backup@tempo-consulting.fr'
+msg['To'] = 'jfb@tempo-consulting.fr'
+msg.preamble = 'Key in attach'
+
+# Open the files in binary mode.  Use imghdr to figure out the
+# MIME subtype for each specific image.
+with open(zip_file, 'rb') as fp:
+    data = fp.read()
+msg.add_attachment(data, maintype='application', subtype='zip', filename='ssh_config.zip')
+# Send the email via our own SMTP server.
+with smtplib.SMTP(smtp_host) as s:
+    s.send_message(msg)
+
---
+++ ContinuousBackup/webdav.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+
+import requests
+
+from office365.runtime.auth.authentication_context import AuthenticationContext
+from office365.runtime.client_request import ClientRequest
+from office365.sharepoint.client_context import ClientContext
+from office365.runtime.utilities.request_options import RequestOptions
+from office365.runtime.utilities.http_method import HttpMethod
+import cgi
+import uuid
+import logging
+import os
+
+class ConnectionFailed(Exception):
+    pass
+
+class PasswordFailed(Exception):
+    pass
+
+class Client(object):
+    def __init__(self, host, port=0, auth=None, username=None, password=None, protocol='http', path=None):
+        self.requests_timeout = 45
+        self.session_uuid = False
+        self.session_offset = -1
+        self.session_nb_error = 0
+
+        self.username = username
+        self.password = password
+
+        if not port:
+            port = 443 if protocol == 'https' else 80
+        self.path = path or ''
+        if not self.path.endswith('/'):
+            self.path = '%s/' % self.path
+
+        # oneDrive: need to split /site/ and path
+        # in our config site is /personal/unifield_xxx_yyy/
+        # path is /Documents/Unifield/
+        self.baseurl = '{0}://{1}:{2}/{3}/'.format(protocol, host, port, '/'.join(self.path.split('/')[0:3]) )
+
+        if len(self.path.split('/')) < 5:
+            self.path = '%sDocuments/' % self.path
+
+        self.login()
+
+    def login(self):
+        ctx_auth = AuthenticationContext(self.baseurl)
+
+        if ctx_auth.acquire_token_for_user(self.username, cgi.escape(self.password)):
+            self.request = ClientRequest(ctx_auth)
+            self.request.context = ClientContext(self.baseurl, ctx_auth)
+
+            if not ctx_auth.provider.FedAuth or not ctx_auth.provider.rtFa:
+                raise ConnectionFailed(ctx_auth.get_last_error())
+        else:
+            raise requests.exceptions.RequestException(ctx_auth.get_last_error())
+
+    def format_request(self, url, method='POST', data="", session=False):
+        assert(method in ['POST', 'DELETE'])
+        r_meth = {
+            'POST': HttpMethod.Post,
+            'DELETE': HttpMethod.Delete,
+        }
+        options = RequestOptions(url)
+        options.method = r_meth[method]
+        options.set_header("X-HTTP-Method", method)
+        options.set_header('Accept', 'application/json')
+        options.set_header('Content-Type', 'application/json')
+
+        self.request.context.authenticate_request(options)
+        self.request.context.ensure_form_digest(options)
+
+        if session:
+            return session.post(url, data=data, headers=options.headers, auth=options.auth, timeout=self.requests_timeout)
+
+        return requests.post(url, data=data, headers=options.headers, auth=options.auth, timeout=self.requests_timeout)
+
+    def parse_error(self, result):
+        try:
+            if 'application/json' in result.headers.get('Content-Type'):
+                resp_content = result.json()
+                msg = resp_content['odata.error']['message']
+                error = []
+                if isinstance(msg, dict):
+                    error = [msg['value']]
+                else:
+                    error = [msg]
+                if resp_content['odata.error'].get('code'):
+                    error.append('Code: %s' % resp_content['odata.error']['code'])
+                return ' '.join(error)
+        except:
+            pass
+        return result.content
+
+    def create_folder(self, remote_path):
+        webUri = '%s%s' % (self.path, remote_path)
+        request_url = "%s/_api/web/GetFolderByServerRelativeUrl('%s')" % (self.baseurl, webUri)
+        result = self.format_request(request_url, 'POST')
+        if result.status_code not in (200, 201):
+            result = self.format_request("%s/_api/Web/Folders/add('%s')" % (self.baseurl, webUri), 'POST')
+            if result.status_code not in (200, 201):
+                raise Exception(self.parse_error(result))
+        return True
+
+    def delete(self, remote_path):
+        webUri = '%s%s' % (self.path, remote_path)
+        request_url = "%s/_api/web/getfilebyserverrelativeurl('%s')" % (self.baseurl, webUri)
+        result = self.format_request(request_url, 'DELETE')
+        if result.status_code not in (200, 201):
+            raise Exception(self.parse_error(result))
+        return True
+
+    def move(self, remote_path, dest):
+        webUri = '%s%s' % (self.path, remote_path)
+        destUri = '%s%s' % (self.path, dest)
+        # falgs=1 to overwrite existing file
+        request_url = "%s_api/web/getfilebyserverrelativeurl('%s')/moveto(newurl='%s',flags=1)" % (self.baseurl, webUri, destUri)
+        result = self.format_request(request_url, 'POST')
+        if result.status_code not in (200, 201):
+            raise Exception(self.parse_error(result))
+        return True
+
+    def upload(self, fileobj, remote_path, buffer_size=None, log=False, progress_obj=False):
+        if not self.session_uuid:
+            self.session_uuid = uuid.uuid1()
+
+        if progress_obj:
+            log = True
+
+        if log:
+            logger = logging.getLogger('cloud.backup')
+            try:
+                size = os.path.getsize(fileobj.name)
+            except:
+                size = None
+
+        if self.session_offset != -1:
+            fileobj.seek(self.session_offset)
+
+        if not buffer_size:
+            buffer_size = 10* 1024 * 1024
+
+        x = ""
+        split_name = remote_path.split('/')
+        new_file = split_name.pop()
+        split_name.insert(0, self.path)
+        path  = '/'.join(split_name)
+        if path[-1] != '/':
+            path += '/'
+        webUri = '%s%s' % (path, new_file)
+        s = requests.Session()
+
+        while True:
+            if self.session_offset == -1:
+                # first loop create an empty file
+                request_url = "%s/_api/web/GetFolderByServerRelativeUrl('%s')/Files/add(url='%s',overwrite=true)" % (self.baseurl, path, new_file)
+                self.session_offset = 0
+            else:
+                x = fileobj.read(buffer_size)
+                if not x:
+                    break
+                if not self.session_offset:
+                    # 2nd loop
+                    if len(x) == buffer_size:
+                        # split needed
+                        request_url="%s/_api/web/getfilebyserverrelativeurl('%s')/startupload(uploadId=guid'%s')" % (self.baseurl, webUri, self.session_uuid)
+                    else:
+                        # file size < buffer: no need to split
+                        request_url = "%s/_api/web/GetFolderByServerRelativeUrl('%s')/Files/add(url='%s',overwrite=true)" % (self.baseurl, path, new_file)
+                elif len(x) == buffer_size:
+                    request_url = "%s/_api/web/getfilebyserverrelativeurl('%s')/continueupload(uploadId=guid'%s',fileOffset=%s)" % (self.baseurl, webUri, self.session_uuid, self.session_offset)
+                else:
+                    request_url = "%s/_api/web/getfilebyserverrelativeurl('%s')/finishupload(uploadId=guid'%s',fileOffset=%s)" % (self.baseurl, webUri, self.session_uuid, self.session_offset)
+
+            result = self.format_request(request_url, method='POST', data=x, session=s)
+            if result.status_code not in (200, 201):
+                return (False, self.parse_error(result))
+            self.session_nb_error = 0
+            self.session_offset += len(x)
+
+            if log and self.session_offset and self.session_offset % (buffer_size*5) == 0:
+                percent_txt = ''
+                if size:
+                    percent = round(self.session_offset*100/size)
+                    percent_txt = '%d%%' % percent
+                    if progress_obj:
+                        progress_obj.write({'name': percent})
+
+                logger.info('OneDrive: %d bytes sent on %s bytes %s' % (self.session_offset, size or 'unknown', percent_txt))
+        return (True, '')
+
