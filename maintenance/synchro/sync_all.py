7f6b13a3bf5e1a5821bd117e42a20c8a575fcc82
jf <jfb@tempo-consulting.fr>
2023-03-22 13:40:14 +0100

    [PY3] sync_all: use xmlrpc

--- maintenance/synchro/sync_all.py
+++ maintenance/synchro/sync_all.py
@@ -2,14 +2,12 @@
 # -*- encoding: utf-8 -*-
 
 import argparse
-import oerplib
+import xmlrpc.client
 import os
-
-protocol='netrpc'
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--host", "-H", metavar="host", default="127.0.0.1", help="Host [default: %(default)s]")
-parser.add_argument("--port", "-p", metavar="port", default=os.getenv('NETRPCPORT',8070), help="NetRPC Port [default: %(default)s]")
+parser.add_argument("--port", "-p", metavar="port", default=os.getenv('XMLRPCPORT',8069), help="XmlRPC Port [default: %(default)s]")
 parser.add_argument("--user", "-u", metavar="user", default="admin", help="User [default: %(default)s]")
 parser.add_argument("--password", "-w", metavar="pwd", default=os.getenv('ADMINPASSWORD','admin'), help="Password [default: %(default)s]")
 
@@ -20,18 +18,19 @@ pwd = o.password
 host = o.host
 port = o.port
 
-oerp = oerplib.OERP(host, protocol=protocol, port=port, version='6.0')
-for dbname in oerp.db.list():
+sock = xmlrpc.client.ServerProxy('http://%s:%s/xmlrpc/db'%(host, port))
+dbs = sock.list()
+
+transport = xmlrpc.client.Transport()
+connection = transport.make_connection(host)
+connection.timeout = 10000
+
+for dbname in sock.list():
     if 'SYNC' in dbname:
         continue
     print("Sync %s" % dbname)
-    netrpc = oerplib.OERP(host, database=dbname, protocol=protocol, port=port, timeout=1000, version='6.0')
-    netrpc.login(login, pwd, dbname)
-    conn_manager = netrpc.get('sync.client.sync_server_connection')
-    conn_ids = conn_manager.search([])
-    #conn_manager.write(conn_ids, {'password': login})
-    #conn_manager.connect()
-    #manager_id = netrpc.get('sync.client.sync_manager').create({})
-    #netrpc.get('sync.client.sync_manager').sync_manual_threaded(manager_id, {})
-    netrpc.get('sync.client.entity').sync()
+    sock = xmlrpc.client.ServerProxy('http://%s:%s/xmlrpc/common' % (host, port))
+    uid = sock.login(dbname, login, pwd)
+    sock = xmlrpc.client.ServerProxy('http://%s:%s/xmlrpc/object' % (host, port), transport=transport)
+    sock.execute(dbname, uid, pwd, 'sync.client.entity', 'sync')
 
